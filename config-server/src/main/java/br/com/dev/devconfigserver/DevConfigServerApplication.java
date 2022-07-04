b0e338b650b727bcd5f872e1f70ad8f1b6091e68
Angelo A. Bezutti <angeloantoniobzt@gmail.com>
2022-07-04 20:20:56 -0300

    Iniciando config-server

---
+++ config-server/src/main/java/br/com/dev/devconfigserver/DevConfigServerApplication.java
@@ -0,0 +1,13 @@
+package br.com.dev.devconfigserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevConfigServerApplication.class, args);
+	}
+
+}
---
+++ config-server/src/test/java/br/com/dev/devconfigserver/DevConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.dev.devconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
