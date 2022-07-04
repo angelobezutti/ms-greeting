7b714b968f1ad870492f4915b5a3467f547244c4
Angelo A. Bezutti <angeloantoniobzt@gmail.com>
2022-07-04 16:08:52 -0300

    Usando valores de configuração

---
+++ src/main/java/br/com/dev/msgretingservice/MsGreetingServiceApplication.java
@@ -0,0 +1,13 @@
+package br.com.dev.msgretingservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsGreetingServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsGreetingServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/dev/msgretingservice/MsGreetingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.dev.msgretingservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsGreetingServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
